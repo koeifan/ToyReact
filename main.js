6cc920e1af77f5c4617564051d33b36d0eb53d2b
koeifan <koeifan@126.com>
2020-08-17 18:41:54 +0800

    看到35分钟

---
+++ main.js
@@ -0,0 +1,11 @@
+for (let i of [1, 2, 3]) {
+    console.log(i);
+}
+function createElement(tagName, attributes, ...children){
+    return document.createElement(tagName);
+}
+let a = <div id="a" class="c">
+    <div></div>
+    <div></div>
+    <div></div>
+</div>
